27d7d15c9d1ff74b281935d3b3e253375f79132b
leong <chao.liang@tincent.me>
2015-12-07 11:38:56 +0800

    add TXBmpUtil.java

---
+++ libtincent/src/com/tincent/android/util/TXBmpUtil.java
@@ -0,0 +1,457 @@
+package com.tincent.android.util;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import junit.framework.Assert;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.util.Log;
+
+/**
+ * 位图处理工具类
+ * 
+ * @author hui.wang
+ * @date 2015.3.16
+ * 
+ */
+public class TXBmpUtil {
+
+	private static final String TAG = "SDK_Sample.Util";
+
+	/**
+	 * 保存位图到文件
+	 * 
+	 * @param bm
+	 * @param path
+	 * @param picName
+	 * @return
+	 */
+	public static String saveBitmap(Bitmap bm, String path, String picName) {
+		try {
+			TXFileUtils.createDir(path);// 创建目录
+			File f = new File(path, picName + ".JPEG");
+			if (f.exists()) {
+				f.delete();
+			}
+			FileOutputStream out = new FileOutputStream(f);
+			bm.compress(Bitmap.CompressFormat.JPEG, 60, out);
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return "";
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "";
+		}
+		return path + picName + ".JPEG";
+	}
+
+	/**
+	 * 位图转换为字节数组
+	 * 
+	 * @param bmp
+	 * @param needRecycle
+	 * @return
+	 */
+	public static byte[] bmpToByteArray(final Bitmap bmp,
+			final boolean needRecycle) {
+		int i;
+		int j;
+		if (bmp.getHeight() > bmp.getWidth()) {
+			i = bmp.getWidth();
+			j = bmp.getWidth();
+		} else {
+			i = bmp.getHeight();
+			j = bmp.getHeight();
+		}
+
+		Bitmap localBitmap = Bitmap.createBitmap(i, j, Bitmap.Config.RGB_565);
+		Canvas localCanvas = new Canvas(localBitmap);
+
+		while (true) {
+			localCanvas.drawBitmap(bmp, new Rect(0, 0, i, j), new Rect(0, 0, i,
+					j), null);
+			if (needRecycle)
+				bmp.recycle();
+			ByteArrayOutputStream localByteArrayOutputStream = new ByteArrayOutputStream();
+			localBitmap.compress(Bitmap.CompressFormat.JPEG, 100,
+					localByteArrayOutputStream);
+			localBitmap.recycle();
+			byte[] arrayOfByte = localByteArrayOutputStream.toByteArray();
+			try {
+				localByteArrayOutputStream.close();
+				return arrayOfByte;
+			} catch (Exception e) {
+				// F.out(e);
+			}
+			i = bmp.getHeight();
+			j = bmp.getHeight();
+		}
+	}
+
+	/**
+	 * 网页转换为字节数组
+	 * 
+	 * @param url
+	 *            网络地址
+	 * @return
+	 */
+	public static byte[] getHtmlByteArray(final String url) {
+		URL htmlUrl = null;
+		InputStream inStream = null;
+		try {
+			htmlUrl = new URL(url);
+			URLConnection connection = htmlUrl.openConnection();
+			HttpURLConnection httpConnection = (HttpURLConnection) connection;
+			int responseCode = httpConnection.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				inStream = httpConnection.getInputStream();
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		byte[] data = inputStreamToByte(inStream);
+
+		return data;
+	}
+
+	/**
+	 * 输入流转换为字节数组
+	 * 
+	 * @param is
+	 * @return
+	 */
+	public static byte[] inputStreamToByte(InputStream is) {
+		try {
+			ByteArrayOutputStream bytestream = new ByteArrayOutputStream();
+			int ch;
+			while ((ch = is.read()) != -1) {
+				bytestream.write(ch);
+			}
+			byte imgdata[] = bytestream.toByteArray();
+			bytestream.close();
+			return imgdata;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	/**
+	 * 从文件中读取字节数组
+	 * 
+	 * @param fileName
+	 * @param offset
+	 * @param len
+	 * @return
+	 */
+	public static byte[] readFromFile(String fileName, int offset, int len) {
+		if (fileName == null) {
+			return null;
+		}
+
+		File file = new File(fileName);
+		if (!file.exists()) {
+			Log.i(TAG, "readFromFile: file not found");
+			return null;
+		}
+
+		if (len == -1) {
+			len = (int) file.length();
+		}
+
+		Log.d(TAG, "readFromFile : offset = " + offset + " len = " + len
+				+ " offset + len = " + (offset + len));
+
+		if (offset < 0) {
+			Log.e(TAG, "readFromFile invalid offset:" + offset);
+			return null;
+		}
+		if (len <= 0) {
+			Log.e(TAG, "readFromFile invalid len:" + len);
+			return null;
+		}
+		if (offset + len > (int) file.length()) {
+			Log.e(TAG, "readFromFile invalid file len:" + file.length());
+			return null;
+		}
+
+		byte[] b = null;
+		try {
+			RandomAccessFile in = new RandomAccessFile(fileName, "r");
+			b = new byte[len];
+			in.seek(offset);
+			in.readFully(b);
+			in.close();
+
+		} catch (Exception e) {
+			Log.e(TAG, "readFromFile : errMsg = " + e.getMessage());
+			e.printStackTrace();
+		}
+		return b;
+	}
+
+	private static final int MAX_DECODE_PICTURE_SIZE = 1920 * 1440;
+
+	/**
+	 * 从文件中获取缩略图
+	 * 
+	 * @param path
+	 * @param height
+	 * @param width
+	 * @param crop
+	 * @return
+	 */
+	public static Bitmap extractThumbNail(final String path, final int height,
+			final int width, final boolean crop) {
+		Assert.assertTrue(path != null && !path.equals("") && height > 0
+				&& width > 0);
+
+		BitmapFactory.Options options = new BitmapFactory.Options();
+
+		try {
+			options.inJustDecodeBounds = true;
+			Bitmap tmp = BitmapFactory.decodeFile(path, options);
+			if (tmp != null) {
+				tmp.recycle();
+				tmp = null;
+			}
+
+			Log.d(TAG, "extractThumbNail: round=" + width + "x" + height
+					+ ", crop=" + crop);
+			final double beY = options.outHeight * 1.0 / height;
+			final double beX = options.outWidth * 1.0 / width;
+			Log.d(TAG, "extractThumbNail: extract beX = " + beX + ", beY = "
+					+ beY);
+			options.inSampleSize = (int) (crop ? (beY > beX ? beX : beY)
+					: (beY < beX ? beX : beY));
+			if (options.inSampleSize <= 1) {
+				options.inSampleSize = 1;
+			}
+
+			// NOTE: out of memory error
+			while (options.outHeight * options.outWidth / options.inSampleSize > MAX_DECODE_PICTURE_SIZE) {
+				options.inSampleSize++;
+			}
+
+			int newHeight = height;
+			int newWidth = width;
+			if (crop) {
+				if (beY > beX) {
+					newHeight = (int) (newWidth * 1.0 * options.outHeight / options.outWidth);
+				} else {
+					newWidth = (int) (newHeight * 1.0 * options.outWidth / options.outHeight);
+				}
+			} else {
+				if (beY < beX) {
+					newHeight = (int) (newWidth * 1.0 * options.outHeight / options.outWidth);
+				} else {
+					newWidth = (int) (newHeight * 1.0 * options.outWidth / options.outHeight);
+				}
+			}
+
+			options.inJustDecodeBounds = false;
+
+			Log.i(TAG, "bitmap required size=" + newWidth + "x" + newHeight
+					+ ", orig=" + options.outWidth + "x" + options.outHeight
+					+ ", sample=" + options.inSampleSize);
+			Bitmap bm = BitmapFactory.decodeFile(path, options);
+			if (bm == null) {
+				Log.e(TAG, "bitmap decode failed");
+				return null;
+			}
+
+			Log.i(TAG,
+					"bitmap decoded size=" + bm.getWidth() + "x"
+							+ bm.getHeight());
+			final Bitmap scale = Bitmap.createScaledBitmap(bm, newWidth,
+					newHeight, true);
+			if (scale != null) {
+				bm.recycle();
+				bm = scale;
+			}
+
+			if (crop) {
+				final Bitmap cropped = Bitmap.createBitmap(bm,
+						(bm.getWidth() - width) >> 1,
+						(bm.getHeight() - height) >> 1, width, height);
+				if (cropped == null) {
+					return bm;
+				}
+
+				bm.recycle();
+				bm = cropped;
+				Log.i(TAG,
+						"bitmap croped size=" + bm.getWidth() + "x"
+								+ bm.getHeight());
+			}
+			return bm;
+
+		} catch (final OutOfMemoryError e) {
+			Log.e(TAG, "decode bitmap failed: " + e.getMessage());
+			options = null;
+		}
+
+		return null;
+	}
+
+	/**
+	 * 图片下载
+	 * 
+	 * @param dir
+	 *            下载文件存储目录
+	 * @param path
+	 *            网络文件路径
+	 * @return 下载的文件
+	 */
+	public File downloadImage(String dir, String path) {
+
+		// http协议连接对象
+		HttpURLConnection conn;
+		try {
+			URL url = new URL(path);// 获取到路径
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");// 这里是不能乱写的，详看API方法
+			conn.setConnectTimeout(6 * 1000);
+			// 别超过10秒。
+			if (conn.getResponseCode() == 200) {
+				InputStream inputStream = conn.getInputStream();
+				String filename = TXMd5Util.md5(url.toString()) + ".jpg";
+				byte[] data = readStream(inputStream);
+				File file = new File(dir + filename);// 给图片起名字
+				FileOutputStream outStream = new FileOutputStream(file);// 写出对象
+				outStream.write(data);// 写入
+				outStream.close(); // 关闭流
+				return file;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+
+	}
+
+	/**
+	 * 读取文件流返回字节码
+	 * 
+	 * @param inStream
+	 * @return
+	 * @throws IOException
+	 */
+	private byte[] readStream(InputStream inStream) throws IOException {
+
+		byte[] buffer = new byte[1024]; // 用数据装װ
+		int len = -1;
+		ByteArrayOutputStream outstream = new ByteArrayOutputStream();
+		while ((len = inStream.read(buffer)) != -1) {
+			outstream.write(buffer, 0, len);
+		}
+		outstream.close();
+		inStream.close();
+		// 关闭流一定要记得。
+		return outstream.toByteArray();
+	}
+
+	/**
+	 * 获取压缩图片
+	 * 
+	 * @param image
+	 * @return
+	 */
+	public static Bitmap calculateInSampleSize(String filePath, int reqWidth, int reqHeight) { // outWidth和outHeight是目标图片的最大宽度和高度，用作限制
+		FileInputStream fs = null;
+		BufferedInputStream bs = null;
+		try {
+			fs = new FileInputStream(filePath);
+			bs = new BufferedInputStream(fs);
+			BitmapFactory.Options options = setBitmapOption(filePath, reqWidth, reqHeight);
+			return BitmapFactory.decodeStream(bs, null, options);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				bs.close();
+				fs.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 设置参数
+	 * @param file
+	 * @param width
+	 * @param height
+	 * @return Options
+	 */
+	private static BitmapFactory.Options setBitmapOption(String file, int width, int height) {
+		BitmapFactory.Options opt = new BitmapFactory.Options();
+		opt.inJustDecodeBounds = true;
+		// 设置只是解码图片的边距，此操作目的是度量图片的实际宽度和高度
+		BitmapFactory.decodeFile(file, opt);
+
+		int outWidth = opt.outWidth; // 获得图片的实际高和宽
+		int outHeight = opt.outHeight;
+		opt.inDither = false;
+		opt.inPreferredConfig = Bitmap.Config.RGB_565;
+		// 设置加载图片的颜色数为16bit，默认是RGB_8888，表示24bit颜色和透明通道，但一般用不上
+		opt.inSampleSize = 1;
+		// 设置缩放比,1表示原比例，2表示原来的四分之一....
+		// 计算缩放比
+//		if (outWidth != 0 && outHeight != 0 && width != 0 && height != 0) {
+//			int sampleSize = (outWidth / width + outHeight / height) / 2;
+//			opt.inSampleSize = sampleSize;
+//		}
+
+		if (height < outHeight || width < outWidth) {
+			final int heightRatio = Math.round((float) outHeight / (float) height);
+			final int widthRatio = Math.round((float) outWidth / (float) width);
+			opt.inSampleSize = Math.max(widthRatio, heightRatio);
+		}
+
+		opt.inJustDecodeBounds = false;// 最后把标志复原
+		return opt;
+	}
+
+	/**
+	 * 
+	 * @param options
+	 * @param reqWidth
+	 * @param reqHeight
+	 * @return
+	 */
+	public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+
+		if (height > reqHeight || width > reqWidth) {
+			final int heightRatio = Math.round((float) height / (float) reqHeight);
+			final int widthRatio = Math.round((float) width / (float) reqWidth);
+			inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+		}
+		return inSampleSize;
+	}
+}
